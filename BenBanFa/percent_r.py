b1ea87b4224c5ea1dd169a7237ccd2a5cc8a418a
MsingWen0222 <1666762170@qq.com>
2017-12-01 23:15:16 +0800

    add

--- BenBanFa/percent_r.py
+++ BenBanFa/percent_r.py
@@ -1,6 +1,9 @@
 #-*-coding:utf-8-*-
 # 用来测试 %r的用例
 
+#	%r用rper()方法处理对象
+#	%s用str()方法处理对象
+
 # 下面3个print语句将不会产生差异
 print "I am %d years old." % 22  
 print "I am %s years old." % 22  
@@ -10,3 +13,5 @@ print "I am %r years old." % 22
 text = "I am %d years old." % 22  
 print "I said: %s." % text  
 print "I said: %r." % text  
+
+#	 可见，%r打印时能够重现它所代表的对象(rper() unambiguously recreate the object it represents) 
