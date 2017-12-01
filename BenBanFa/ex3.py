51048a5bf5b7a9de12a5b9868c1d6d9122d930e8
MsingWen0222 <1666762170@qq.com>
2017-12-01 20:55:39 +0800

    add

---
+++ BenBanFa/ex3.py
@@ -0,0 +1,25 @@
+
+#-*-coding:utf-8-*-
+print "I will now count my chickens:"
+print "Hens", 25 + 30.0 / 6	#30
+print "Roosters", 100 - 25.0 * 3 % 4 # 97
+print "Now i will count the eggs"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0/ 4 + 6 #7 	6.75
+print "Is it true that 3 + 2 < 5 - 7 ?" # false
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?" , 3 + 2	# 5
+print "What is 5 - 7?", 5 - 7 # -2
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 # true
+print "Is it greater or eaqual", 5 >= -2  # true
+print "Is it less or equal.", 5 <= -2 # false
+
+
+# 自己写一个测试用例
+s = "5"
+print "我爱你";
+print "Hello",s
