4415e30dee2dfe7f04148fbd3f6bfbed29212567
MsingWen0222 <1666762170@qq.com>
2017-12-01 20:52:38 +0800

    add directory

---
+++ IntroducingPython/Unit/1/Test1.py
@@ -0,0 +1,16 @@
+
+"""
+列表
+"""
+cichles = [
+    "At the end of the day",
+    "Having said that",
+    "The fact of the matter is",
+    "Be that as it may",
+    "The bottom line is",
+    "If you will",
+]
+
+
+# 5是偏移量的意思
+print(cichles[5])
