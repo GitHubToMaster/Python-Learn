d32d0e9448b68738d42fb921b86efe5cd1d0eed8
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-02 13:27:02 +0800

    add

--- BenBanFa/ex11.py
+++ BenBanFa/ex11.py
@@ -1,3 +1,5 @@
+
+#-*-coding:utf-8-*-
 print "How old are you?",
 age = raw_input();
 
@@ -10,3 +12,5 @@ print "How much do you weight?",
 weight = raw_input();
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+## 注意:每一行
