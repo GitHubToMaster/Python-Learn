217e883dcd60ab721639ec51e9f21b59e6ef6b2d
sivanWu0222 <sivan0222@vip.qq.com>
2017-12-02 15:14:08 +0800

    modify ex11.py

--- BenBanFa/ex11.py
+++ BenBanFa/ex11.py
@@ -14,3 +14,5 @@ weight = raw_input();
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
 
 ## 注意:每一行print后面都加了一个逗号(comma),这样之后print就不会输出新行符而结束一行符跑到下一行去了
+
+## 注意:这里最后一行 '6\'2"' 里边有一个\'序列,单引号需要被转义,从而防止它被识别为字符串的结尾.
